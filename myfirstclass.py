5ee3a74902ce84a0707cfdcbeabc5356a6d60eae
zshashz <72076441+zshashz@users.noreply.github.com>
2021-10-09 18:00:33 +0530

    Update myfirstclass.py

--- myfirstclass.py
+++ myfirstclass.py
@@ -1,1 +1,12 @@
-# Make changes to this file.
+import time
+import webbrowser
+
+total_breaks=10
+break_count=0
+
+print("This Program Started on " + time.ctime())
+
+while(break_count < total_breaks):
+    time.sleep(5)
+    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    break_count=break_count+1
